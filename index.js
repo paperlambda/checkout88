4ae34653f42ba9507eb8f8b5078d2b896fd04ed8
paperlambda <revzankamarullah@gmail.com>
2018-12-14 00:31:47 +0700

    Change addon extend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const EngineAddon = require('ember-engines/lib/engine-addon');
 
-module.exports = EngineAddon({
+module.exports = EngineAddon.extends({
   name: 'checkout88',
 	lazyLoading: false
 });
